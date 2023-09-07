27383eef6077872bc7511d5e8818de9afb841c43
Erythrocyte3803 <2544390577@qq.com>
2023-09-07 13:15:43 +0900

    添加主角名字恢复功能

--- 4_Get_DiffSinger_Label.py
+++ 4_Get_DiffSinger_Label.py
@@ -9,11 +9,13 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--source', type=str, help='未整理数据集目录', required=True)
 parser.add_argument('--index', type=str, help='索引路径', required=True)
 parser.add_argument('--dest', type=str, help='目标路径', required=True)
+parser.add_argument('--lang', type=str, help='语言（可选CHS/EN/JP/KR，默认为CHS）', default="CHS")
 args = parser.parse_args()
 
 source = str(args.source)
 dest = str(args.dest)
 index = str(args.index)
+lang = str(args.lang)
 filter = 'fetter|battle|life|monster'
 
 renameDict = {}
@@ -36,6 +38,31 @@ def has_vaild_content(text):
         return True
     else:
         return False
+    
+def ren_player(player,lang):
+    p_name = player
+    if p_name == "playerboy" or p_name == "playergirl":
+        if p_name == "playerboy":
+            if lang == "CHS":
+                p_name = "开拓者(男)"
+            if lang == "EN":
+                p_name = "Trailblazer(M)"
+            if lang == "JP":
+                p_name = "開拓者(男)"
+            if lang == "KR":
+                p_name = "개척자(남)"
+        if p_name == "playergirl":
+            if lang == "CHS":
+                p_name = "开拓者(女)"
+            if lang == "EN":
+                p_name = "Trailblazer(F)"
+            if lang == "JP":
+                p_name = "開拓者(女)"
+            if lang == "KR":
+                p_name = "개척자(여)"
+    else:
+        p_name = player
+    return p_name
 
 f = open(index, encoding='utf8')
 data = json.load(f)
@@ -45,9 +72,9 @@ for k in tq.tqdm(data.keys()):
         char_name = data.get(k).get('Speaker')
         title_text = data.get(k).get('TitleText')
         if char_name is not None:
-            char_name = char_name
+            char_name = ren_player(char_name,lang)
         else:
-            char_name = title_text
+            char_name = ren_player(title_text,lang)
         if char_name in renameDict:
             char_name = renameDict[char_name]
         path = data.get(k).get('VoiceName')
