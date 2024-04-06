bc4c417a72084d18d833a61ea58462f9d2e58217
Erythrocyte3803 <2544390577@qq.com>
2024-04-06 20:55:05 +0900

    1

--- 6_Second_Sorting.py
+++ 6_Second_Sorting.py
@@ -44,32 +44,35 @@ def tag_content(text):
     return string
 
 for file in tqdm(labfiles):
-    lab_content = Path(file).read_text(encoding='utf-8')
-    spk = os.path.basename(os.path.dirname(file))
-    lab_file_name = os.path.basename(file)
-    wav_file_name = lab_file_name.replace(".lab",".wav")
-    src = f"{source}/{spk}"
-    if check_content(lab_content,tags):
-        labels = re.sub(r'<.*?>', '', lab_content)
-        lab_path = f"{src}/{lab_file_name}"
-        Path(lab_path).write_text(labels,encoding='utf-8')
-        tqdm.write(f"已清除标注文件 {src}/{lab_file_name} 中的html标签：{tag_content(lab_content)}\n-----------")
-    if not check_content(lab_content,vaild_content):
-        out_path = f"{dest}/{spk}/其它语音 - Others"
-    elif is_in(file,conv):
-        out_path = f"{dest}/{spk}/多人对话 - Conversation" 
-    elif check_content(lab_content,placeholder):
-        out_path = f"{dest}/{spk}/带变量语音 - Placeholder"
-    elif is_in(file,monster):
-        out_path = f"{dest}/{spk}/怪物语音 - Monster"
-    elif is_in(file,battle):
-        out_path = f"{dest}/{spk}/战斗语音 - Battle"
-    else:
-        continue
-    if not os.path.exists(out_path):
-        Path(f"{out_path}").mkdir(parents=True)
-    move(f"{src}/{lab_file_name}",f"{out_path}/{lab_file_name}")
-    move(f"{src}/{wav_file_name}",f"{out_path}/{wav_file_name}")
-    tqdm.write(f"音频文件：{src}/{wav_file_name}\n标注文件：{src}/{lab_file_name}\n已移动至：{out_path}\n-----------")
+    try:
+        lab_content = Path(file).read_text(encoding='utf-8')
+        spk = os.path.basename(os.path.dirname(file))
+        lab_file_name = os.path.basename(file)
+        wav_file_name = lab_file_name.replace(".lab",".wav")
+        src = f"{source}/{spk}"
+        if check_content(lab_content,tags):
+            labels = re.sub(r'<.*?>', '', lab_content)
+            lab_path = f"{src}/{lab_file_name}"
+            Path(lab_path).write_text(labels,encoding='utf-8')
+            tqdm.write(f"已清除标注文件 {src}/{lab_file_name} 中的html标签：{tag_content(lab_content)}\n-----------")
+        if not check_content(lab_content,vaild_content):
+            out_path = f"{dest}/{spk}/其它语音 - Others"
+        elif is_in(file,conv):
+            out_path = f"{dest}/{spk}/多人对话 - Conversation" 
+        elif check_content(lab_content,placeholder):
+            out_path = f"{dest}/{spk}/带变量语音 - Placeholder"
+        elif is_in(file,monster):
+            out_path = f"{dest}/{spk}/怪物语音 - Monster"
+        elif is_in(file,battle):
+            out_path = f"{dest}/{spk}/战斗语音 - Battle"
+        else:
+            continue
+        if not os.path.exists(out_path):
+            Path(f"{out_path}").mkdir(parents=True)
+        move(f"{src}/{lab_file_name}",f"{out_path}/{lab_file_name}")
+        move(f"{src}/{wav_file_name}",f"{out_path}/{wav_file_name}")
+        tqdm.write(f"音频文件：{src}/{wav_file_name}\n标注文件：{src}/{lab_file_name}\n已移动至：{out_path}\n-----------")
+    except:
+        pass
 
     
